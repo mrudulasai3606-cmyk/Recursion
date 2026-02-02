07992c8ee12707351fb43e526ea640106f2b5fa4
mrudula <mrudulasai3606@gmail.com>
2026-02-02 14:07:43 +0530

    Changed sum of digits in return

--- src/SumOfDigits.java
+++ src/SumOfDigits.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class SumOfDigits {
     public static int Sum(int n){
-        if(n==0) return 046576;
+        if(n==0) return 0;
         return n%10 + Sum(n/10);
     }
     public static void main(String[] args) {
